2bfacbbf6b9c75178fefaba9e0d5915877d522e7
alenchu <alain.k.prevot@gmail.com>
2022-08-31 21:30:22 -0300

    prueba 5

--- src/assets/Components/TarjetaNFT.jsx
+++ src/assets/Components/TarjetaNFT.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import StyleTarjetaNFT from './style/TarjetaNFT.module.sass'
 
 import ImageEquilibrium from '../../image/TarjetaNFT/ImageEquilibrium.jpg'
+import ImageAvatar from '../../image/TarjetaNFT/ImageAvatar.png'
 
 
 const TarjetaNFT = ({title, item, description, price, money, time, author}) => {
@@ -10,7 +11,7 @@ const TarjetaNFT = ({title, item, description, price, money, time, author}) => {
         <div className={StyleTarjetaNFT.card}>
             <img className={StyleTarjetaNFT.card__image} src={ImageEquilibrium} alt="imageEquilibrium" />
             <div className={StyleTarjetaNFT.card__content}>
-                {/* <a>{title} #{item}</a> */}
+                <a>{title} #{item}</a>
                 <p>{description}</p>
                 <div>
                     <h3>{price} {money}</h3>
@@ -18,7 +19,7 @@ const TarjetaNFT = ({title, item, description, price, money, time, author}) => {
                 </div>
             </div>
             <div className={StyleTarjetaNFT.card__content__user}>
-                <img className={StyleTarjetaNFT.avatar} src="../../../src/image/TarjetaNFT/ImageAvatar.png" alt="imageAvatar" />
+                <img className={StyleTarjetaNFT.avatar} src={ImageAvatar} alt="imageAvatar" />
                 <h2>Creado por <a href="">{author}</a></h2>
             </div>
         </div>
