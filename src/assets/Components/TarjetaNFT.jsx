3e26a13426b9d1e379ded9016bac9368d3b62f68
alenchu <alain.k.prevot@gmail.com>
2022-08-31 21:21:55 -0300

    prueba 2

--- src/assets/Components/TarjetaNFT.jsx
+++ src/assets/Components/TarjetaNFT.jsx
@@ -8,7 +8,7 @@ import StyleTarjetaNFT from './style/TarjetaNFT.module.sass'
 const TarjetaNFT = ({title, item, description, price, money, time, author}) => {
     return(
         <div className={StyleTarjetaNFT.card}>
-            <img className={StyleTarjetaNFT.card__image} src="../../../src/image/TarjetaNFT/ImageEquilibrium.jpg" alt="imageEquilibrium" />
+            <img className={StyleTarjetaNFT.card__image} src="../../image/TarjetaNFT/ImageEquilibrium.jpg" alt="imageEquilibrium" />
             <div className={StyleTarjetaNFT.card__content}>
                 <a>{title} #{item}</a>
                 <p>{description}</p>
